2d27d481dfb3eb45511897fb662d9fc27bbd5650
Karl Wu <wuchanghui5220@gmail.com>
2019-11-22 14:52:05 +0800

    fix all bug

--- game.py
+++ game.py
@@ -2,21 +2,16 @@
 
 a = 7
 c = 3
-print("Welcome to guess game!")
+
+# print(type(number))
 while c > 0:
-        number = int(input("Please input a number: ")
-	if number > a:
-		print("Too Big")
-	elif number < a:
-		print("Too small")
-	else:
-		print("Well done, you did it")
-		break
-	c -= 1
-	print("You have " + str(c) + " chances left. ")
-print("Have Fun, See you!")
-
-print("^_^" * 3)
-
-# This game is Fun.
-print("Today is a good day")
+    number = int(input("Please input a number: "))
+    if number == a:
+        print("Yes, you did it")
+        break
+    elif number <= a:
+        print("Too small")
+    else:
+        print("Too Big")
+    c -= 1
+    print("You have " + str(c) + "chances left. ")
